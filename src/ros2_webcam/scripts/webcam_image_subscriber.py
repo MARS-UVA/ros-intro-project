b3f9bfa2259ae918b0de95574cb83819fde0e682
Surya Shanmugaselvam <112454589+Surya-Selvam@users.noreply.github.com>
2024-09-29 13:03:49 -0400

    Update webcam_image_subscriber.py added comments

--- src/ros2_webcam/scripts/webcam_image_subscriber.py
+++ src/ros2_webcam/scripts/webcam_image_subscriber.py
@@ -1,30 +1,36 @@
 #!/usr/bin/env python3
 
+# ROS Python Libraries
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Image
+
+# OpenCV imports, use 'pip install opencv-python' to get OpenCV for Python
 from cv_bridge import CvBridge
 import cv2
 
+# Webcam Subscriber node which inherits the Node parent class from rclpy
 class WebcamSubscriber(Node):
     def __init__(self) -> None:
-        super().__init__("webcam_subscriber")
+        super().__init__("webcam_subscriber") # Calling parents class to assign node name 
         self.get_logger().info("Webcam subscriber node init")
-        self.subscription = self.create_subscription(
+        self.subscription = self.create_subscription(  #Creating a subscription with a callback
             Image,
             'webcam_topic',
             self.listener_callback,
-            10
+            10  # This number which you see in the C++ publisher nodes as well is the queue size, that is how many messages to keep in the queue (subscriber queue in this case). Any message that exceeds the queue will be dicarded
         )
-        self.bridge = CvBridge()
-    
-    def listener_callback(self, msg):
-        cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
+        self.bridge = CvBridge()  #Instantiating a CVBridge instance
 
-        cv2.imshow("Image", cv_image)
+    #Callback function called when a message is ready to be processed from the subscriber queue
+    def listener_callback(self, msg):
+        cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')  #Convert image to OpenCV matrix
+
+        cv2.imshow("Image", cv_image)    #Use imshow() to display image
         cv2.waitKey(1)
     
 
+# Main method is the first point of entry which instantiates an instance of the WebcamSubscriber class (a child of the Node class)
 def main(args=None):
     rclpy.init()
     node = WebcamSubscriber()
