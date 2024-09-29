f05c579e60e5c3b9a48daaffc61144ec905e61c0
mars_host <mars_host@mars-lunabotics.myguest.virtualbox.org>
2024-09-29 12:23:36 -0400

    renamed pkg

---
+++ src/ros2_keyboard/scripts/keypress_publisher.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import keyboard  #run 'pip install keyboard' in your terminal to get keyboard
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+
+class KeypressPublisher(Node):
+    def __init__(self):
+        super().__init__('keypress_publisher')
+        self.publisher_ = self.create_publisher(String, 'key_pressed', 10)
+        self.read_timer = self.create_timer(0.1, self.read_keypress_callback)
+
+    def read_keypress_callback(self):
+        key_pressed = keyboard.read_event()
+        msg = String()
+        msg.data = f'{key_pressed.name}'
+        self.publisher_.publish(msg)
+        self.get_logger().info(f'Published data: "{msg.data}"')
+
+if __name__ == '__main__':
+    rclpy.init()
+    node = KeypressPublisher()
+    rclpy.spin(node)
+    rclpy.shutdown()
