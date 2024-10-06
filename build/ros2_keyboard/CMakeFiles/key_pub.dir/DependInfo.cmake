2fc2c0565a4a707abeea75b616248ddedfe5a59b
Tony Lin <hpm3ep@virginia.edu>
2024-10-06 14:46:05 -0400

    push

---
+++ build/ros2_keyboard/CMakeFiles/key_pub.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/mars_host/ros-intro-project/src/ros2_keyboard/src/key_publisher.cpp" "CMakeFiles/key_pub.dir/src/key_publisher.cpp.o" "gcc" "CMakeFiles/key_pub.dir/src/key_publisher.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/ros2_webcam/scripts/image_transform.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+import cv2
+from sensor_msgs.msg import Image
+from std_msgs.msg import String
+from cv_bridge import CvBridge
+
+class ImageTransform(Node):
+    def __init__(self):
+        super().__init__('image_transform')
+        self.publisher_ = self.create_publisher(Image, 'transformed_image_topic', 10)
+        self.imageSubscription = self.create_subscription(
+            Image,
+            'webcam_topic',
+            self.image_callback,
+            10
+        )
+        self.keypressSubscription = self.create_subscription(
+            String,
+            'keypress_topic',
+            self.keypress_callback,
+            10)
+        self.bridge = CvBridge()
+        self.greyscale = False
+    def keypress_callback(self, msg):
+        self.get_logger().info('Received keypress: "%s"' % msg.data)
+        if msg.data == 'k':
+            self.greyscale = not self.greyscale
+    def image_callback(self, msg):
+        cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
+        if self.greyscale:
+            cv_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
+            ros_image = self.bridge.cv2_to_imgmsg(cv_image, encoding='mono8')
+        else:
+            ros_image = self.bridge.cv2_to_imgmsg(cv_image, encoding='bgr8')
+        self.publisher_.publish(ros_image)
+ 
+
+def main(args=None):
+    rclpy.init(args=args)
+    minimal_publisher = ImageTransform()
+    rclpy.spin(minimal_publisher)
+    minimal_publisher.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
